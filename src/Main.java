94189c3b1ff68de3e3c64ae2493f929db72122c6
Shawn Luo <shawn.luotianyu@gmail.com>
2015-03-17 18:34:17 +0800

    HTTP or SSH?

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Another test for HTTP or SSH");
         System.out.println("Hello World!");
     }
 }
