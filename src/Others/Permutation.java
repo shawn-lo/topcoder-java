125aaccc585e5f283e7a1ebc033c9f8de8f7b7ed
Shawn Luo <shawn.luotianyu@gmail.com>
2015-04-28 22:15:48 +0800

    Permutaion. Java Pass-By-Value

--- src/Others/Permutation.java
+++ src/Others/Permutation.java
@@ -4,11 +4,11 @@ package Others;
  * Created by Shawn on 4/27/15.
  */
 public class Permutation {
-    public void swap(int a, int b)
+    public void swap(int[] array, int i, int j)
     {
-        int temp = b;
-        b = a;
-        a = temp;
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
     }
 
     public void printArray(int[] array)
@@ -17,6 +17,7 @@ public class Permutation {
         {
             System.out.printf("%d ", array[i]);
         }
+        System.out.println(" ");
     }
 
     public void getPermutation(int[] array, int startIndex, int endIndex)
@@ -29,10 +30,14 @@ public class Permutation {
         {
             for(int i = startIndex; i <= endIndex; i++)
             {
-                swap(array[startIndex], array[i]);
+                swap(array, startIndex, i);
                 getPermutation(array, startIndex+1, endIndex);
-                swap(array[startIndex], array[i]);
+                swap(array, startIndex, i);
             }
         }
+//        printArray(array);
+//        swap(array, 0, 1);
+//        System.out.println(" ");
+//        printArray(array);
     }
 }
