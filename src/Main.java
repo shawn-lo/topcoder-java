3a8a2ac73020ba8dd706c3c8cad2209713e8f6fd
Shawn Luo <shawn.luotianyu@gmail.com>
2015-04-30 15:55:04 +0800

    Permutation with Dup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import BruteForce.*;
 import Others.Permutation;
-import StringParsing.*;
-import DP.*;
 
 public class Main {
 
@@ -241,7 +238,13 @@ public class Main {
  * Permutation
  */
         Permutation t = new Permutation();
-        int[] array = {1, 3, 5};
+        int[] array = {1, 2, 2};
         t.getPermutation(array, 0, 2);
+/**
+ * Permutation No Duplication
+ */
+//        PermutationDup t = new PermutationDup();
+//        int[] array = {1, 3, 2};
+//        t.getPermtDup(array, 0, 2);
     }
 }
---
+++ src/Others/PermutationDup.java
@@ -0,0 +1,53 @@
+package Others;
+
+/**
+ * Created by Shawn on 4/30/15.
+ */
+public class PermutationDup {
+
+    public void printArray(int[] array)
+    {
+        for(int x : array)
+        {
+            System.out.printf("%d ", x);
+        }
+        System.out.println(" ");
+    }
+
+    public void swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public boolean isDup(int[] array, int start, int end)
+    {
+        for(int i = start; i < end; i++)
+        {
+            if(array[start] == array[end])
+                return true;
+        }
+        return false;
+    }
+
+    public void getPermtDup(int[] array, int startIndex, int endIndex)
+    {
+        if(startIndex == endIndex)
+        {
+            printArray(array);
+        }
+        else
+        {
+            for(int i = startIndex; i <= endIndex; i++)
+            {
+//                if(!isDup(array, startIndex, i))
+//                {
+                    swap(array, startIndex, i);
+                    getPermtDup(array, startIndex+1, i);
+                    swap(array, startIndex, i);
+//                }
+            }
+        }
+    }
+}
